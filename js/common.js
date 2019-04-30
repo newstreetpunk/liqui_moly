93b6d84e49c6505c24117c0f2daed93076d0b1d0
newstreetpunk <newstreetpunk@yandex.ru>
2019-04-30 10:32:31 +0500

    Сверстана секция слайдеров + адаптив

--- js/common.js
+++ js/common.js
@@ -1,5 +1,29 @@
 $(function() {
 
-	// Custom JS
+	$('.promo-slider').slick({
+		speed: 300,
+		autoplay: true,
+  		autoplaySpeed: 2000,
+  		responsive:[
+  		{
+  			breakpoint: 768,
+  			settings: {
+  				arrows: false
+  			}
+  		}]
+	});
+
+	$('.promo-slider--man').slick({
+		speed: 300,
+		autoplay: true,
+  		autoplaySpeed: 2000,
+  		responsive:[
+  		{
+  			breakpoint: 768,
+  			settings: {
+  				arrows: false
+  			}
+  		}]
+	});
 
 });
